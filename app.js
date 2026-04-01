141a9108f94a64c2b3e94eda24a1c6203bee9a86
taatanova-del <ta.atanova@gmail.com>
2026-04-01 02:26:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const user = users.find(u => u.username === username && u.password === password);
+  if(user){
+    localStorage.setItem("currentUser", JSON.stringify(user));
+    window.location.href = "main.html";
+  } else {
+    document.getElementById("login-error").textContent = "Неверный логин или пароль";
+  }
+}
